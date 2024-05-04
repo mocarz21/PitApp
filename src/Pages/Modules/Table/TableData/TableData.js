c693a3d7fd657bd6652047c92bebf518b3d6985a
Adam <adammocarski22@gmail.com>
2024-05-04 09:27:18 +0200

    Add filter table

--- src/Pages/Modules/Table/TableData/TableData.js
+++ src/Pages/Modules/Table/TableData/TableData.js
@@ -1,127 +1,147 @@
-import { useMemo, useState } from 'react';
-import { MaterialReactTable, useMaterialReactTable, MRT_GlobalFilterTextField, MRT_ToggleFiltersButton} from 'material-react-table';
-import {Box, Button, ListItemIcon, MenuItem, Typography, lighten } from '@mui/material';
-import { AccountCircle, Send } from '@mui/icons-material';
-//import { data } from './makeData';
+import { useMemo } from 'react';
+import {
+  MRT_GlobalFilterTextInput,
+  MRT_ToggleFiltersButton,
+  MantineReactTable,
+  useMantineReactTable,
+} from 'mantine-react-table';
+import { Box, Button, Flex, Menu, Text, Title } from '@mantine/core';
+import { IconUserCircle, IconSend } from '@tabler/icons-react';
+import { data } from './data';
 
 export const TableData = () => {
-  const [data, setData] =useState=(  {
-    firstName: 'John',
-    lastName: 'Doe',
-    email: 'john.doe@example.com',
-    avatar: 'https://via.placeholder.com/150',
-    salary: 65000,
-    jobTitle: 'Frontend Developer',
-    startDate: '2021-04-10',
-    signatureCatchPhrase: 'Keep it simple.'
-  },
-  {
-    firstName: 'Jane',
-    lastName: 'Smith',
-    email: 'jane.smith@example.com',
-    avatar: 'https://via.placeholder.com/150',
-    salary: 72000,
-    jobTitle: 'Full Stack Developer',
-    startDate: '2019-08-23',
-    signatureCatchPhrase: 'To the moon and back.'
-  })
-
-
   const columns = useMemo(
     () => [
       {
         id: 'employee', //id used to define `group` column
-        header: 'Employee',
+        header: 'Referencje',
         columns: [
           {
-            accessorFn: (row) => `${row.firstName} ${row.lastName}`, //accessorFn used to join multiple data into a single cell
+            accessorFn: (row) => `${row.nazwa}`, //accessorFn used to join multiple data into a single cell
             id: 'name', //id is still required when using accessorFn instead of accessorKey
-            header: 'Name',
+            header: 'Nazwa',
             size: 250,
+            filterVariant: 'autocomplete',
             Cell: ({ renderedCellValue, row }) => (
               <Box
                 sx={{
                   display: 'flex',
                   alignItems: 'center',
-                  gap: '1rem',
+                  gap: '16px',
                 }}
               >
-                <img
-                  alt="avatar"
-                  height={30}
-                  src={row.original.avatar}
-                  loading="lazy"
-                  style={{ borderRadius: '50%' }}
-                />
-                {/* using renderedCellValue instead of cell.getValue() preserves filter match highlighting */}
-                <span>{renderedCellValue}</span>
+                <span>{renderedCellValue}</span> 
               </Box>
             ),
           },
           {
-            accessorKey: 'email', //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
-            enableClickToCopy: true,
+            accessorFn: (row) => `${row.projekt}`, //accessorFn used to join multiple data into a single cell
+            id: 'project-name', //id is still required when using accessorFn instead of accessorKey
+            header: 'Nazwa Projektu',
+            size: 250,
             filterVariant: 'autocomplete',
-            header: 'Email',
-            size: 300,
+            Cell: ({ renderedCellValue, row }) => (
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '16px',
+                }}
+              >
+                <span>{renderedCellValue}</span> 
+              </Box>
+            ),
+          },
+          {
+            accessorFn: (row) => `${row.firma}`, //accessorFn used to join multiple data into a single cell
+            id: 'zamawiajacy', //id is still required when using accessorFn instead of accessorKey
+            header: 'Zamawiający',
+            size: 250,
+            filterVariant: 'autocomplete',
+            Cell: ({ renderedCellValue, row }) => (
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '16px',
+                }}
+              >
+                <span>{renderedCellValue}</span> 
+              </Box>
+            ),
+          },
+          {
+            accessorFn: (row) => `${row.skills}`, //accessorFn used to join multiple data into a single cell
+            id: 'skills', //id is still required when using accessorFn instead of accessorKey
+            header: 'Tematyka',
+            size: 250,
+            filterVariant: 'autocomplete',
+            Cell: ({ renderedCellValue, row }) => (
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '16px',
+                }}
+              >
+                <span>{renderedCellValue}</span> 
+              </Box>
+            ),
+          },
+          
+          {
+            accessorFn: (row) => `${row.firma}`, //accessorFn used to join multiple data into a single cell
+            id: 'beneficiary', //id is still required when using accessorFn instead of accessorKey
+            header: 'Beneficjenci',
+            size: 250,
+            filterVariant: 'multi-select',
+            Cell: ({ renderedCellValue, row }) => (
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '16px',
+                }}
+              >
+                <span>{renderedCellValue}</span> 
+              </Box>
+            ),
           },
         ],
       },
       {
         id: 'id',
-        header: 'Job Info',
+        header: 'Data',
         columns: [
           {
-            accessorKey: 'salary',
-            // filterVariant: 'range', //if not using filter modes feature, use this instead of filterFn
-            filterFn: 'between',
-            header: 'Salary',
-            size: 200,
-            //custom conditional format and styling
-            Cell: ({ cell }) => (
-              <Box
-                component="span"
-                sx={(theme) => ({
-                  backgroundColor:
-                    cell.getValue() < 50_000
-                      ? theme.palette.error.dark
-                      : cell.getValue() >= 50_000 && cell.getValue() < 75_000
-                        ? theme.palette.warning.dark
-                        : theme.palette.success.dark,
-                  borderRadius: '0.25rem',
-                  color: '#fff',
-                  maxWidth: '9ch',
-                  p: '0.25rem',
-                })}
-              >
-                {cell.getValue()?.toLocaleString?.('en-US', {
-                  style: 'currency',
-                  currency: 'USD',
-                  minimumFractionDigits: 0,
-                  maximumFractionDigits: 0,
-                })}
-              </Box>
-            ),
-          },
-          {
-            accessorKey: 'jobTitle', //hey a simple column for once
-            header: 'Job Title',
-            size: 350,
-          },
-          {
-            accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
+            accessorFn: (row) => {
+              //convert to Date for sorting and filtering
+              const sDay = new Date(row.od);
+              sDay.setHours(0, 0, 0, 0); // remove time from date (useful if filter by equals exact date)
+              return sDay;
+            },
             id: 'startDate',
-            header: 'Start Date',
-            filterVariant: 'date',
-            filterFn: 'lessThan',
+            header: 'Data od',
+            filterVariant: 'date-range',
             sortingFn: 'datetime',
+            enableColumnFilterModes: false, //keep this as only date-range filter with between inclusive filterFn
             Cell: ({ cell }) => cell.getValue()?.toLocaleDateString(), //render Date as a string
             Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
-            muiFilterTextFieldProps: {
-              sx: {
-                minWidth: '250px',
-              },
+          },
+          {
+            accessorFn: (row) => {
+              //convert to Date for sorting and filtering
+              const sDay = new Date(row.do);
+              sDay.setHours(0, 0, 0, 0); // remove time from date (useful if filter by equals exact date)
+              return sDay;
             },
+            id: 'endDate',
+            header: 'Data do',
+            filterVariant: 'date-range',
+            sortingFn: 'datetime',
+            enableColumnFilterModes: false, //keep this as only date-range filter with between inclusive filterFn
+            Cell: ({ cell }) => cell.getValue()?.toLocaleDateString(), //render Date as a string
+            Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
           },
         ],
       },
@@ -129,91 +149,55 @@ export const TableData = () => {
     [],
   );
 
-  const table = useMaterialReactTable({
+  const table = useMantineReactTable({
     columns,
-    data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+    data, //must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
     enableColumnFilterModes: true,
     enableColumnOrdering: true,
-    enableGrouping: true,
-    enableColumnPinning: true,
     enableFacetedValues: true,
-    enableRowActions: true,
+    enableGrouping: true,
+    enablePinning: true,
+    enableRowActions: false,
     enableRowSelection: true,
-    initialState: {
-      showColumnFilters: true,
-      showGlobalFilter: true,
-      columnPinning: {
-        left: ['mrt-row-expand', 'mrt-row-select'],
-        right: ['mrt-row-actions'],
-      },
-    },
+    initialState: { showColumnFilters: true, showGlobalFilter: true },
     paginationDisplayMode: 'pages',
     positionToolbarAlertBanner: 'bottom',
-    muiSearchTextFieldProps: {
-      size: 'small',
-      variant: 'outlined',
+    mantinePaginationProps: {
+      radius: 'xl',
+      size: 'lg',
     },
-    muiPaginationProps: {
-      color: 'secondary',
-      rowsPerPageOptions: [10, 20, 30],
-      shape: 'rounded',
-      variant: 'outlined',
+    mantineSearchTextInputProps: {
+      placeholder: 'Wyszukaj',
     },
     renderDetailPanel: ({ row }) => (
       <Box
         sx={{
-          alignItems: 'center',
           display: 'flex',
-          justifyContent: 'space-around',
-          left: '30px',
-          maxWidth: '1000px',
-          position: 'sticky',
-          width: '100%',
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          gap: '16px',
+          padding: '16px',
         }}
       >
         <img
           alt="avatar"
           height={200}
           src={row.original.avatar}
-          loading="lazy"
           style={{ borderRadius: '50%' }}
         />
         <Box sx={{ textAlign: 'center' }}>
-          <Typography variant="h4">Signature Catch Phrase:</Typography>
-          <Typography variant="h1">
-            &quot;{row.original.signatureCatchPhrase}&quot;
-          </Typography>
+          <Title>Opis:</Title>
+          <Text>&quot;{row.original.projekt}&quot;</Text>
+          <Text>&quot;{row.original.skills.join(', ')}&quot;</Text>
         </Box>
       </Box>
     ),
-    renderRowActionMenuItems: ({ closeMenu }) => [
-      <MenuItem
-        key={0}
-        onClick={() => {
-          // View profile logic...
-          closeMenu();
-        }}
-        sx={{ m: 0 }}
-      >
-        <ListItemIcon>
-          <AccountCircle />
-        </ListItemIcon>
-        View Profile
-      </MenuItem>,
-      <MenuItem
-        key={1}
-        onClick={() => {
-          // Send email logic...
-          closeMenu();
-        }}
-        sx={{ m: 0 }}
-      >
-        <ListItemIcon>
-          <Send />
-        </ListItemIcon>
-        Send Email
-      </MenuItem>,
-    ],
+    renderRowActionMenuItems: () => (
+      <>
+        <Menu.Item icon={<IconUserCircle />}>View Profile</Menu.Item>
+        <Menu.Item icon={<IconSend />}>Send Email</Menu.Item>
+      </>
+    ),
     renderTopToolbar: ({ table }) => {
       const handleDeactivate = () => {
         table.getSelectedRowModel().flatRows.map((row) => {
@@ -234,53 +218,43 @@ export const TableData = () => {
       };
 
       return (
-        <Box
-          sx={(theme) => ({
-            backgroundColor: lighten(theme.palette.background.default, 0.05),
-            display: 'flex',
-            gap: '0.5rem',
-            p: '8px',
-            justifyContent: 'space-between',
-          })}
-        >
-          <Box sx={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
+        <Flex p="md" justify="space-between">
+          <Flex gap="xs">
             {/* import MRT sub-components */}
-            <MRT_GlobalFilterTextField table={table} />
+            <MRT_GlobalFilterTextInput table={table} />
             <MRT_ToggleFiltersButton table={table} />
-          </Box>
-          <Box>
-            <Box sx={{ display: 'flex', gap: '0.5rem' }}>
-              <Button
-                color="error"
-                disabled={!table.getIsSomeRowsSelected()}
-                onClick={handleDeactivate}
-                variant="contained"
-              >
-                Deactivate
-              </Button>
-              <Button
-                color="success"
-                disabled={!table.getIsSomeRowsSelected()}
-                onClick={handleActivate}
-                variant="contained"
-              >
-                Activate
-              </Button>
-              <Button
-                color="info"
-                disabled={!table.getIsSomeRowsSelected()}
-                onClick={handleContact}
-                variant="contained"
-              >
-                Contact
-              </Button>
-            </Box>
-          </Box>
-        </Box>
+          </Flex>
+          <Flex sx={{ gap: '8px' }}>
+            <Button
+              color="red"
+              disabled={!table.getIsSomeRowsSelected()}
+              onClick={handleDeactivate}
+              variant="filled"
+            >
+              Deactivate
+            </Button>
+            <Button
+              color="green"
+              disabled={!table.getIsSomeRowsSelected()}
+              onClick={handleActivate}
+              variant="filled"
+            >
+              Activate
+            </Button>
+            <Button
+              color="blue"
+              disabled={!table.getIsSomeRowsSelected()}
+              onClick={handleContact}
+              variant="filled"
+            >
+              Contact
+            </Button>
+          </Flex>
+        </Flex>
       );
     },
   });
 
-  return <MaterialReactTable table={table} />;
+  return <MantineReactTable table={table} />;
 };
 
---
+++ src/Pages/Modules/Table/TableData/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    nazwa: 'Joseph',
+    projekt: 'przyszłosc',
+    od: '2022-12-28T20:59:36.586Z' ,
+    do: '2024-12-28T20:59:36.586Z',
+    firma: "closed",
+    beneficjent: 'osoby bezdomne',
+    jobTitle: 'szsz',
+    startDate: '2022-12-28T20:59:36.586Z',
+    skills: ['Excel', 'Word'],
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/528.jpg',
+  },
+  {
+    nazwa: 'Joseph',
+    projekt: 'przyszłosc',
+    od: '2021-12-28T20:59:36.586Z' ,
+    do: '2023-12-28T20:59:36.586Z',
+    firma: "open",
+    beneficjent: 'osoby bezdomne',
+    jobTitle: 'szsz',
+    skills: [ 'Word'],
+    avatar:
+      'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/528.jpg',
+  }
+]
