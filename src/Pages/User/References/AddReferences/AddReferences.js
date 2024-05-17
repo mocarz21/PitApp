2162b3d178e7132cfa4aaca57910f11efe1ac9df
Adam <adammocarski22@gmail.com>
2024-05-17 20:43:08 +0200

    Add rorm to add references

--- src/Pages/User/References/AddReferences/AddReferences.js
+++ src/Pages/User/References/AddReferences/AddReferences.js
@@ -1,16 +1,34 @@
 import { useState } from "react"
 import './AddReferences.scss'
 
+
+
 export const AddReferences = () => {
   const [imagePreview, setImagePreview] = useState(null);
+  const [imgName , setImgName] = useState()
+  const [name , setName] = useState()
+  const [projectName , setProjectName] = useState()
+  const [company , setCompany] = useState()
+  const [thema , setThema] = useState()
+  const [beneficiary , setBeneficiary] = useState()
+  const [startDate , setStartDate] = useState()
+  const [endDate , setEndDate] = useState()
 
+  let data 
+
+
+  
   const handleSubmit = (event) => {
-    event.preventDefault(); // Zapobiega domyślnemu odświeżeniu strony
-    console.log('działa');
+    event.preventDefault();
+    data={name, projectName, company, thema, beneficiary, startDate, endDate }
+    console.log(imgName)
   };
 
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
+
+
+  const handleInputFile=(e)=>{
+    const file = e.target.files[0];
+    setImgName(file.name)
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
@@ -18,67 +36,61 @@ export const AddReferences = () => {
       };
       reader.readAsDataURL(file);
     }
-  };
+  }
 
   return (
     <div className="container add-references">
       <div className="form-header">
         <h1>Add References</h1>
-        <div>
-          <label className="form-label">
-            Wybierz plik:
-            <input 
-              className="form-input" 
-              type="file" 
-              onChange={handleImageChange} 
-              style={{ display: 'block' }}
-            />
-          </label>
-        </div>
+
       </div>
       <div className="form-content">
         <div className="form-section">
           <form onSubmit={handleSubmit}>
             <div className="form-group">
               <label className="form-label">
+                Wybierz plik:
+                <input className="form-input" type="file" onChange={handleInputFile} />
+              </label>
+              <label className="form-label">
                 NAZWA:
-                <input className="form-input" type="text" name="nazwa"/>
+                <input className="form-input" type="text" name="nazwa" value={name} onChange={(e)=>setName(e.target.value)}/>
               </label>
             </div>
             <div className="form-group">
               <label className="form-label">
                 PROJEKT:
-                <input className="form-input" type="text" name="projekt"/>
+                <input className="form-input" type="text" name="projekt" value={projectName} onChange={(e)=>setProjectName(e.target.value)} />
               </label>
             </div>
             <div className="form-group">
               <label className="form-label">
                 FIRMA:
-                <input className="form-input" type="text" name="firma"/>
+                <input className="form-input" type="text" name="firma" value={company} onChange={(e)=>setCompany(e.target.value)} />
               </label>
             </div>
             <div className="form-group">
               <label className="form-label">
                 TEMATYKA:
-                <input className="form-input" type="text" name="tematyka"/>
+                <input className="form-input" type="text" name="tematyka" value={thema} onChange={(e)=>setThema(e.target.value)} />
               </label>
             </div>
             <div className="form-group">
               <label className="form-label">
                 BENEFICJENT:
-                <input className="form-input" type="text" name="beneficjent"/>
+                <input className="form-input" type="text" name="beneficjent" value={beneficiary} onChange={(e)=>setBeneficiary(e.target.value)} />
               </label>
             </div>
             <div className="form-group">
               <label className="form-label">
                 OD:
-                <input className="form-input" type="date" name="od"/>
+                <input className="form-input" type="date" name="od" value={startDate} onChange={(e)=>setStartDate(e.target.value)}/>
               </label>
             </div>
             <div className="form-group">
               <label className="form-label">
                 DO:
-                <input className="form-input" type="date" name="do"/>
+                <input className="form-input" type="date" name="do" value={endDate} onChange={(e)=>setEndDate(e.target.value)}/>
               </label>
             </div>
             <button type="submit">Dodaj</button>
@@ -86,7 +98,7 @@ export const AddReferences = () => {
         </div>
         <div className="image-section">
           {imagePreview && (
-            <img className="image-preview" src={imagePreview} alt="Preview" />
+            <img className="image-preview" src={imagePreview} alt="Preview"  />
           )}
         </div>
       </div>
